3778f9337b4df3b100a3432bf0fde4a693322679
Geovany Gameros <geovanygameros@gmail.com>
2018-12-06 20:43:13 -0700

    Se implementa la interfaz de cada propiedad

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -13,7 +13,16 @@ class PropertiesController < ApplicationController
     ###################### Se reciben los parámetros
     properties_loaded = params[:properties_loaded].to_i
     rooms = params[:search_object][:rooms]
+    min_price = params[:search_object][:min_price]
+    max_price = params[:search_object][:max_price]
     @properties = Property.where.not(status: Property::DELETED)
+
+    #filtro por número de cuartos
+    @properties = @properties.where(rooms: rooms) unless rooms.blank?
+
+    #filtro por precio
+    @properties = @properties.where('price > ?',min_price) unless min_price.blank?
+    @properties = @properties.where('price < ?',max_price) unless max_price.blank?
 
     respond_to do |format|
       format.html do
@@ -22,4 +31,9 @@ class PropertiesController < ApplicationController
       end
     end
   end
+
+  def show
+    @property = Property.find(params[:id])
+  end
+
 end
